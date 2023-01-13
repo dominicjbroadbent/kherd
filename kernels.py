d27d9b9dec22ada67b0f693957d9fb03c2367190
Dominic Broadbent <tt21864@bristol.ac.uk>
2023-01-13 14:32:52 +0000

    Create kernels.py

---
+++ kernels.py
@@ -0,0 +1,23 @@
+import numpy as np
+from scipy.spatial.distance import cdist
+
+class BaseKernel:
+    def __init__(self):
+        """Initialise the kernel instance"""
+        pass
+    
+    def __call__(self, x, y):
+        """Evaluate the kernel function at (x, y)"""
+        raise NotImplementedError()
+    
+            
+class Gaussian(BaseKernel):
+    def __init__(self, lengthscale = 0.5):
+        """Initialise the Gaussian kernel"""
+        self.lengthscale = lengthscale
+        super().__init__()
+        
+    def __call__(self, x, y):
+        """Evaluate the kernel function at (x, y)"""
+        sq_diffs = cdist(x, y, metric = 'sqeuclidean')
+        return np.exp( - 0.5 * sq_diffs / self.lengthscale)
